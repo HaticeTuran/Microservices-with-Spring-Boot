def3c4fbb5668e4b7c13f3efdfbef86c3eb78137
Hatice <e190503011@stud.tau.edu.tr>
2022-07-25 11:40:51 +0300

    added mew module "comment"

---
+++ comment/src/test/kotlin/com/example/comment/CommentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.comment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
