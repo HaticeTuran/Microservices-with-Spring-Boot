841253a007930b5c4c7000b106246ad4db46e807
Hatice <e190503011@stud.tau.edu.tr>
2022-07-29 15:01:26 +0300

    added new files Config and sql in Module product

---
+++ product/src/main/resources/db/migration/postgres/V0__init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS public.products
+(
+    product_id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    product_name character varying NOT NULL,
+    list_price integer,
+    quantity_in_stock integer,
+    product_description character varying NOT NULL,
+    created_date time with time zone NOT NULL,
+    CONSTRAINT products_pkey PRIMARY KEY (product_id)
+)
