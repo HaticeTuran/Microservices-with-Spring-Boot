28a7e475f33caec8508b89ad21e83c2af5b5ee40
Hatice <e190503011@stud.tau.edu.tr>
2022-08-05 12:02:49 +0300

    test for bannerService run successfully

--- banner/src/test/kotlin/com/example/banner/service/BannerServiceTest.kt
+++ banner/src/test/kotlin/com/example/banner/service/BannerServiceTest.kt
@@ -6,24 +6,24 @@ import com.example.banner.bannerManagement.BannerService
 import io.mockk.coEvery
 import org.junit.jupiter.api.Test
 import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import java.util.UUID
 
 class BannerServiceTest {
 
-    val bannerRepository = mockk<BannerRepository>()
-    val bannerService = BannerService(bannerRepository)
+    private val bannerRepository = mockk<BannerRepository>()
+    private val bannerService = BannerService(bannerRepository)
 
     @Test
-    fun `when BannerService findBannerById called it should return Banner`(){
-        //TODO
+    fun `when BannerService findBannerById called it should return Banner`()= runBlocking{
         //Given
         val bannerId =UUID.randomUUID()
         val expected = Banner(bannerId,bannerUrl = "hello",createdDate = null)
+        coEvery { bannerRepository.findById(bannerId)} returns expected
 
         //When
         val actual: Banner? = bannerService.findBannerById(bannerId)
-        coEvery { bannerRepository.findById(bannerId)} returns expected
 
         //Then
         assertEquals(expected,actual)
